e0efcb1380fb9a5dbd1babf2f8aa11e2e8038e32
Tyler <nemurbrains@gmail.com>
2021-01-23 20:16:44 -0700

    Started setting up node.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const bodyParser = require('body-parser')
+const PORT = process.env.PORT || 7000
+
+app.use(express.static(path.join(__dirname, 'public')))
+// Body parser stuff
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false }));
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('pages/index'))
+app.post('/math', math)
+app.post('/math_service', math_service)
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
