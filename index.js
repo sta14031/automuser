58a8a5c36cba7571a28960ef143ae5126fd044a1
Tyler <nemurbrains@gmail.com>
2021-02-03 18:04:11 -0700

    Added header for navigation

--- index.js
+++ index.js
@@ -11,4 +11,5 @@ app.use(bodyParser.urlencoded({extended: false }));
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.get('/', (req, res) => res.render('pages/index'))
+app.get('/about', (req, res) => res.render('pages/about'))
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
